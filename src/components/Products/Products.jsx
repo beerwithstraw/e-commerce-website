3ea2a2f44c9dbe109d107eef2e5c35e11b2d6c08
beerwithstraw <agarwalp@tcd.ie>
2020-12-14 03:09:15 +0000

    Product component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,8 +3,8 @@ import Grid from '@material-ui/core';
 import { ShortTextSharp } from '@material-ui/icons';
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running Shoes'},
-    {id: 2, name: 'Airpods', description: 'Apple Airpods Pro'},
+    {id: 1, name: 'Shoes', description: 'Running Shoes', price: '$50'},
+    {id: 2, name: 'Airpods', description: 'Apple Airpods Pro', price: '$250'},
 ];
 
 
@@ -12,7 +12,9 @@ const Products = () => {
     <main>
         <Grid container justify="center" spacing={4} >
             {products.map((product) => (
-                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}></Grid>
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product}  />
+                </Grid>
             ))}
 
         </Grid>
