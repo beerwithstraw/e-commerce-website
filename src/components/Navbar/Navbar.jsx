a1de7cad50f230f08a64256b3c6f7b0f5d62ea03
beerwithstraw <agarwalp@tcd.ie>
2020-12-19 20:33:17 +0000

    Navbar with styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import logo from '../../assets/commerce.png';
+import classes from '*.module.css';
 const Navbar = () => {
     return (
-        <div>
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="E-Commerce Website" height="25px" className={classes.image}/> 
+                        E-Commerce Website
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show Cart Items" color="inherit">
+                            <Badge badgeContent={2} color="secondary" >
+                                <ShoppingCart />    
+                            </Badge>    
+                        </IconButton>    
+                    </div> 
+                </Toolbar>
+
+            </AppBar>
             
-        </div>
+        </>
     )
 }
 
