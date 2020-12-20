5bfdca1dcf72cf596b02718748129d40455a9e49
beerwithstraw <agarwalp@tcd.ie>
2020-12-20 03:13:55 +0000

    env file for pk in commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,7 +7,6 @@ import useStyles from './styles'
 const products = [
     {id: 1, name: 'Shoes', description: 'Running Shoes', price: '$50', image: 'https://image.cnbcfm.com/api/v1/image/105680013-1547583426762nike1.jpg?v=1547583682'},
     {id: 2, name: 'Airpods', description: 'Apple Airpods Pro', price: '$250', image: 'https://slickdeals.net/blog/wp-content/uploads/2020/01/19-airpods-pro-daniel-9.jpg'},
-    {id: 3, name: 'Airpods', description: 'Apple Airpods Pro', price: '$250', image: 'https://slickdeals.net/blog/wp-content/uploads/2020/01/19-airpods-pro-daniel-9.jpg'},
 
 
 ];
