934124995c8580c6bcc2c489c934ae1ac578c336
beerwithstraw <agarwalp@tcd.ie>
2020-12-21 20:40:04 +0000

    Basic checkout Forms

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -2,19 +2,32 @@ import React, { useState } from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 import useStyles from './styles'
+import AddressForm from '../AddressForm'
+import PaymentForm from '../PaymentForm'
 
 const steps = ['Shipping Address', 'Payment Details'];
 
 const Checkout = () => {
-    const [activeStep, setActiveStep] = useState(0);
+    const [activeStep, setActiveStep] = useState(2);
     const classes = useStyles()
+
+    const Confirmation = () => (
+        <div>
+            Confirmation 
+        </div>
+    )
+
+    const Form = () => activeStep === 0
+        ? <AddressForm /> 
+        : <PaymentForm />
+
     return (
         <>
             <div className={classes.toolbar} />
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
                     <Typography variant="h4" align="center">Checkout</Typography>
-                    <Stepper activeStep={0} className={classes.stepper}>
+                    <Stepper activeStep={activeStep} className={classes.stepper}>
                         {steps.map((step) =>(
                             <Step key={step}>
                                 <StepLabel>{step}</StepLabel>
@@ -22,7 +35,7 @@ const Checkout = () => {
                         ))}
 
                     </Stepper>
-
+                    { activeStep === steps.length ? <Confirmation /> : Form }
                 </Paper>
               
             </main>  
