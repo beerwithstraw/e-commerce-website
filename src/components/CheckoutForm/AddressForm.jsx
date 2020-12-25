46f3bf3c07ba5efe2c7133e8d69d8410715ab2c9
beerwithstraw <agarwalp@tcd.ie>
2020-12-25 20:43:39 +0000

    Shipping Countries for Product

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
+import { commerce } from '../../lib/commerce'
+
 import FormInput from './CustomTextField';
-const AddressForm = () => {
+const AddressForm = ({ checkoutToken}) => {
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
+    const [shippingOptions, setShippingOptions] = useState([]);
+    const [shippingOption, setShippingOption] = useState('');
+    
     const methods = useForm();
+    const countries = Object.entries(shippingCountries).map(([code, name]) => ({id: code, label: name}))
+
+
+    const fetchShippingCountries = async (checkoutTokenId) => {
+        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+        console.log(countries)
+        setShippingCountries(countries);
+        setShippingCountry(Object.keys(countries)[0])
+    }
+
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id);
+    }, [])
 
     return (
         <>
@@ -18,7 +40,17 @@ const AddressForm = () => {
                         <FormInput required name="email" label="Email" />
                         <FormInput required name="city" label="City" />
                         <FormInput required name="zip" label="Zip / Postal code" /> 
-                   </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                                {countries.map((country) => (
+                                    <MenuItem key={country.id} value={country.id}>
+                                        {country.label}
+                                    </MenuItem> 
+                                ))}
+                            </Select>
+                        </Grid>
+                    </Grid>
                 </form>
             </FormProvider> 
         </>
